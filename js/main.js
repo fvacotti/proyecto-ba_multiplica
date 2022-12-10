ef5d2fd6492c8d5089e5f1efefcef195c232e837
fvacotti <fvacotti@oulook.com>
2022-12-10 17:42:01 -0300

    -

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+// --------------- MENÚ HAMBURGUESA ---------------
 
-navToggle.addEventListener("click", () => {navMenu.classList.toggle ("nav-menu_visible")});
+const menu = document.querySelector(".nav-menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+	menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+
+const menuLinks = document.querySelectorAll('.nav-menu a[href^="#"]');
+
+const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			const id = entry.target.getAttribute("id");
+			const menuLink = document.querySelector(`.nav-menu a[href="#${id}"]`);
+
+			if (entry.isIntersecting) {
+				document.querySelector(".nav-menu a.selected").classList.remove("selected");
+				menuLink.classList.add("selected");
+			}
+		});
+	},
+
+	// para que no se intersecten elementos que estén muy juntos
+	
+	{ rootMargin: "-30% 0px -70% 0px" }
+);
+
+menuLinks.forEach((menuLink) => {
+	menuLink.addEventListener("click", function () {
+		menu.classList.remove("menu_opened");
+	});
+
+	const hash = menuLink.getAttribute("href");
+	const target = document.querySelector(hash);
+	if (target) {
+		observer.observe(target);
+	}
+});
 
 // --------------- BOTÓN ARRIBA ---------------
 
 function subir() {
-	window.addEventListener("scroll", () =>{
+	window.addEventListener("scroll", () => {
 		let scroll = document.documentElement.scrollTop;
 		let btnUp = document.getElementById("btn-up");
 
-		if(scroll >= 300){
+		if (scroll >= 300) {
 			btnUp.style.right = 30 + "px";
 		} else {
 			btnUp.style.right = -200 + "px";
@@ -90,7 +129,7 @@ for (let i = 0; i < galleryItem.length; i++) {
 
 // slider
 
-function sliderImg (n) {
+function sliderImg(n) {
 	showLightbox(index += n);
 }
 
@@ -108,8 +147,8 @@ lightBoxNext.addEventListener("click", nextImg);
 
 // cerrar galería
 
-function closeLightBox () {
-	if(this === event.target){
+function closeLightBox() {
+	if (this === event.target) {
 		lightBoxContainer.style.display = "none"
 	}
 }
